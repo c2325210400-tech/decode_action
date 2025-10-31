c4c3267db95a4f12ab72eec0cfd5a707fa1d0338
action <action@github.com>
2025-10-31 12:18:05 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,9 +1,33 @@
-//Fri Oct 31 2025 12:14:38 GMT+0000 (Coordinated Universal Time)
+//Fri Oct 31 2025 12:18:05 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-const s = (t, r) => {
-  const o = t.__vccOpts || t;
-  for (const [c, e] of r) o[c] = e;
-  return o;
-};
-export { s as _ };
+import { k as e, c as n, i as t, a as o } from "./index-wKIbGhGo.js";
+const r = {
+    xmlns: "http://www.w3.org/2000/svg",
+    "xmlns:xlink": "http://www.w3.org/1999/xlink",
+    viewBox: "0 0 512 512"
+  },
+  s = o("path", {
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    "stroke-width": "32",
+    d: "M256 112v288"
+  }, null, -1),
+  i = o("path", {
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+    "stroke-width": "32",
+    d: "M400 256H112"
+  }, null, -1),
+  l = [s, i],
+  h = e({
+    name: "Add",
+    render: function (c, a) {
+      return t(), n("svg", r, l);
+    }
+  });
+export { h as A };
